4cb817f1a271804599ad2a0fe86234973ff41a02
MeghaUppin <uppinmegha96@gmail.com>
2022-10-13 15:10:15 +0100

    Adding UI tests

---
+++ Virgin Money StaffUITests/Virgin_Money_StaffUITests.swift
@@ -0,0 +1,25 @@
+//
+//  Virgin_Money_StaffUITests.swift
+//  Virgin Money StaffUITests
+//
+//  Created by Megha on 11/10/22.
+//
+
+import XCTest
+
+class Virgin_Money_StaffUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
---
+++ Virgin Money StaffUITests/Virgin_Money_StaffUITestsLaunchTests.swift
@@ -0,0 +1,21 @@
+//
+//  Virgin_Money_StaffUITestsLaunchTests.swift
+//  Virgin Money StaffUITests
+//
+//  Created by Megha on 11/10/22.
+//
+
+import XCTest
+
+class Virgin_Money_StaffUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    
+}
